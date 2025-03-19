7dfe386adf51f9f170db4e8ea528efe90ec9daf8
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-03-20 01:29:26 +0530

    navbar design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+  <>
+            <header className='max-w-screen-2x1 mx-auto px-4 py-6'>
+                    <nav className='flex justify-between items-center'>
+                    {/* left side dive */}
+                        <div>
+                            <Link to="/">Logo</Link>
+                        </div>
+
+                        {/* right side dive */}
+                        <div>
+                            nav items
+                        </div>
+                    </nav>
+            </header>
+  
+  
+  
+  </>
   )
 }
 
-export default Navbar
+export default Navbar;
