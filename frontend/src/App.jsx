68b020b7ed7cb12c21177e2efc960423f9123c63
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-03-20 01:30:10 +0530

    Navbar component add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-    <nav>Navbar</nav>         {/* Navbar and footer will not change ,it will static in always,only outlet element (routers) will dyanamic(render)*/}
+      <Navbar/>        {/* Navbar and footer will not change ,it will static in always,only outlet element (routers) will dyanamic(render)*/}
       
       <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary'>
           <Outlet />  
