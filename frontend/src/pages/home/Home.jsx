68a2d9077d56818bd31fbe74b91fce7a05fa6c51
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-03-28 23:53:56 +0530

    import recommended section component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
 import Topsellers from './Topsellers'
-
+import Recommened from './Recommened'
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Topsellers/>
+        <Recommened/>
     </div>
   )
 }
