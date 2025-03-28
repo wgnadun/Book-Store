de4cdb06e025cfd1ceb602c4c9649757cdab529e
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-03-28 12:37:57 +0530

    create the BookCard(inprogess)

--- frontend/src/pages/home/Topsellers.jsx
+++ frontend/src/pages/home/Topsellers.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect ,useState} from 'react'
+import BookCard from '../books/BookCard';
+
+const categories =["Choose a genre","Bussiness","Fiction","Horror","Adventure"];
 
 const Topsellers = () => {
     const [books,setBooks] = useState([]);
+    const [selectedCategory,setSelectedCategory] = useState("Choose a genre");
 
     useEffect(() => {
             fetch("books.json")
@@ -9,10 +13,33 @@ const Topsellers = () => {
             .then((data)=>setBooks(data));
 
     }, []);
-            console.log(books);
+    
+    const filteredBooks = selectedCategory === "Choose a genre" ? books :books.filter(book =>book.category === selectedCategory.toLowerCase());
+
+    console.log(filteredBooks);
+            
   return (
-    <div>
-       Topsellers
+    <div className='py-10'>
+       <h1 className='text-3xl font-semibold mb-6'>Top Selller</h1>
+  
+  {/* Filter Systeme for Category */}
+        <div className='mb-8 flex items-center'>
+            <select onChange={(e)=>setSelectedCategory(e.target.value)}
+                
+                name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
+                  {
+                    categories.map((category,index)=>(
+
+                      <option key={index} value={category}>{category}</option>
+                    ))
+                  }  
+            </select>
+        </div>
+        {
+          filteredBooks.map((book,index)=>(
+              <BookCard key={index} book={book}/>
+          ))
+        }
     </div>
   )
 }
