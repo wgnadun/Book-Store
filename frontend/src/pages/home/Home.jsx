34a2b943e2b871db82bf16114078d5e5f5cbec54
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-01 12:28:19 +0530

    complete news section ,(adding swiper sliders)

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from './Banner'
 import Topsellers from './Topsellers'
 import Recommened from './Recommened'
+import News from './News'
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Topsellers/>
         <Recommened/>
+        <News/>
     </div>
   )
 }
