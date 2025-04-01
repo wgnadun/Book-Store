21f8918c39464852e4a4119d0d0a15f48218d4e4
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-01 13:35:45 +0530

    define login and registration routings

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,7 +1,8 @@
 import {createBrowserRouter} from 'react-router-dom';
 import App from '../App';
 import Home from '../pages/home/Home';
-
+import Login from '../components/Login';
+import Register from '../components/Register';
 const router =createBrowserRouter([
     {
         path: '/',
@@ -18,6 +19,12 @@ const router =createBrowserRouter([
             {
                 path:'/about',
                 element:<div>About</div>
+            },{
+                path:'/login',
+                element:<Login/> // Login component, you can replace it with your own component
+            },{
+                path:'/register',
+                element:<Register/> // Register component, you can replace it with your own component
             }
         ]
     },
