d0da69acb3e4cf89d5c474af6f31c6a99b4b1537
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-01 13:55:38 +0530

    login container(box shadow)

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+            <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+                <h2 className='text-xl font-semibold mb-4'>Please Login !</h2>
+            </div>
     </div>
   )
 }
