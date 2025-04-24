4cceccdfd9eb330713ba7faaee18671d7a12606c
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-24 16:33:16 +0530

    add cart items

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';  
+import cartReducer from "./features/cart/cartSlice";
 
 export const store = configureStore({
-    reducer: {}, 
+    reducer: {
+        cart:cartReducer
+    }, 
     
-});
+})
         
 
 
