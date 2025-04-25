7973ad416e47331079b4af2cac2e03a732a7c25b
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-25 17:33:43 +0530

    create cart add to cart and already exists popus using seetalert2 components

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit'
+import  Swal  from 'sweetalert2';
 
 const initialState = {
     cartItems :[]
@@ -13,9 +14,22 @@ const cartSlice = createSlice({
             const existingItem = state.cartItems.find(item =>item._id === action.payload._id)
             if(!existingItem){
                 state.cartItems.push(action.payload);
-                alert("Item added succesfully !")
-            } else {
-                alert("Item already exists !");
+                Swal
+                .fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Successfully added to the cart !",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });            } else {
+                    Swal.fire({
+                        title: " Oops !",
+                        text:"This products is already exists your in Cart !",
+                        icon: "warning",
+                        showConfirmButton:true,
+                        confirmButtonColor: "#E84B3D",
+                        iconColor:"#d33"
+                      });
             }
         }
     }
