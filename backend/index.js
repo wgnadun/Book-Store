de662ebffe0a73c3cb6698c80330aed890b35ae6
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-27 19:52:47 +0530

    install nodemon package and setup

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Nad Nasfdsfsf!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
