4c0822965146b649087aa5802d599e6d2b1d87a3
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-27 21:11:21 +0530

    install nodemon package and mogodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Nad Nasfdsfsf!');
-})
+// plbg9DNqSAXQ1kkI
+
+
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+
+  app.use('/', (req, res) => {
+    res.send('Nad Nasfdsfsf!');
+  });  
+}
+
+main().then(()=>console.log("Mongodb connect successfuly")).catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
