c7520b36400ef556f883fd03adbfd4e30e0f26da
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-28 17:51:32 +0530

    write a routings and controllers for get single book and updated a single element

--- backend/src/books/book.controller.js
+++ backend/src/books/book.controller.js
@@ -25,7 +25,42 @@ const postABook =async(req,res)=>{
         res.status(500).send({message:"failed to fetch books"})
     }
  }
+// get single book controller func
+ 
+ const getSingleBook = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const book = await Book.findById(id);
+        if(!book){
+            res.status(404).send({message:" oops ! Book is not found !"})
+        }
+        res.status(200).send({book})
+    }catch(err){
+        console.error("Error in fetching books",err)
+        res.status(500).send({message:"failed to fetch a book"})
+    }
+ }
+// Update  book controller func
+ 
+ const updateBook = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const updatedbook = await Book.findByIdAndUpdate(id,req.body, {new:true});
+        if(!updatedbook){
+            res.status(404).send({message:" oops ! Book is not found !"})
+        }
+        res.status(200).send({
+            message:"Book updated successfully",
+            book:updatedbook
+        })
+    }catch(err){
+        console.error("Error in fetching books",err)
+        res.status(500).send({message:"failed to fetch a book"})
+    }
+ }
 module.exports={
     postABook,
-    getAllBooks
+    getAllBooks,
+    getSingleBook,
+    updateBook
 }
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const book = require('./book.model');
-const { postABook,getAllBooks } = require('./book.controller');
+const { postABook,getAllBooks,getSingleBook,updateBook } = require('./book.controller');
 
 // post a book
 router.post("/create-book",postABook)
 
 // get all books
 router.get("/",getAllBooks)
+
+// get single  book
+router.get("/:id",getSingleBook)
+
+// update a single  book
+router.get("/edit/:id",updateBook)
 
 module.exports = router;
