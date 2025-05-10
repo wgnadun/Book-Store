c51c8cbf72a34c9058ce440d1d7302755c8f3260
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-05-10 19:58:56 +0530

    add verifyAdminToken for post a book

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { postABook,getAllBooks,getSingleBook,updateBook,deleteBook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 
 // post a book
-router.post("/create-book",postABook)
+router.post("/create-book",verifyAdminToken,postABook)
 
 // get all books
 router.get("/",getAllBooks)
