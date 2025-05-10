1dac97646c0c219aadb59b0427e89479be16ad52
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-05-10 19:25:26 +0530

    apply jwt to admin authentications

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const User = require('./user.model');
+const jwt = require('jsonwebtoken');
+
 
 const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET_KEY
 
 router.post('/admin',async (req,res)=>{
     const {username,password} = req.body;
     try {
         const admin = await User.findOne({username})
+        if(!admin){
+            res.status(404).send({message:"Admin not found!"})
+        }
+        if(admin.password !== password){
+            return res.status(401).send({message:'Invalid password'})
+        }
+     
+        const token = jwt.sign(
+            {id:admin._id,username:admin.username,role:admin.role}, // payload (data to be encoded)
+            JWT_SECRET,         // secret key
+            {expiresIn:'1h'} // token expires in 1 hour
+        )
+
+        return res.status(200).json({
+            message :"Authentication successful",
+            token: token,
+            user : {
+                username:admin.username,
+                role:admin.role
+            }
+        })
     } catch (error) {
         console.error("Failed to login as admin",error)
         res.status(401).send({message:"Failed to login as admin"})
