d0cf98786717add304f6eb00725cfbba383a87a4
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-05-12 20:11:40 +0530

    fetch all books and handlDeleteBok and set the navigations

--- frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
+++ frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useDeleteBookMutation } from '../../../redux/features/books/booksApi';
 
 const ManageBooks = () => {
-  return (
-    <div>
-      ManageBooks
-    </div>
-  )
-}
+    const navigate = useNavigate();
 
+    const {data: books, refetch} = useFetchAllBooksQuery()
+
+    const [deleteBook] = useDeleteBookMutation()
+
+    // Handle deleting a book
+    const handleDeleteBook = async (id) => {
+        try {
+            await deleteBook(id).unwrap();
+            alert('Book deleted successfully!');
+            refetch();
+
+        } catch (error) {
+            console.error('Failed to delete book:', error.message);
+            alert('Failed to delete book. Please try again.');
+        }
+    };
+
+    // Handle navigating to Edit Book page
+    const handleEditClick = (id) => {
+        navigate(`dashboard/edit-book/${id}`);
+    };
+
+    return (
+    <>
+    
+    </>
+    )
+}
 export default ManageBooks
